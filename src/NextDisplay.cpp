e46c4f7fcf140a1799bd166a55455a66d5817c31
João Álvaro Oliveira Silva <91163460+JoaoAlv4ro@users.noreply.github.com>
2024-06-14 19:00:10 -0300

    Add files via upload

---
+++ src/NextDisplay.cpp
@@ -0,0 +1,59 @@
+#include "NextDisplay.h"
+#include "NextDisplayFont.cpp" // Inclui a definição da matriz de dados das letras
+
+// Construtor da classe
+NextDisplay::NextDisplay() {
+  // Configura os pinos das linhas como saída
+  for (int i = 0; i < 8; i++) {
+    pinMode(rows[i], OUTPUT);
+    digitalWrite(rows[i], HIGH); // Inicializa em desligado (HIGH, porque estamos usando LOW para ligar)
+  }
+  // Configura os pinos das colunas como saída
+  for (int j = 0; j < 5; j++) {
+    pinMode(cols[j], OUTPUT);
+    digitalWrite(cols[j], LOW); // Inicializa desligado (LOW)
+  }
+}
+
+// Exibir uma letra na matriz de LEDs
+void NextDisplay::displayLetter(int letterIndex) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(rows[i], LOW); // Liga a linha
+    for (int j = 0; j < 5; j++) {
+      if (alphabet[letterIndex][i][j] == 1) {
+        digitalWrite(cols[j], HIGH); // Liga a coluna
+      }
+      delay(1); // Pequena demora para que o LED acenda de uma forma mais visivel
+      digitalWrite(cols[j], LOW); // Desliga a coluna
+    }
+    digitalWrite(rows[i], HIGH); // Desliga a linha
+  }
+}
+
+// Exibe a letra escolhida na matriz
+void NextDisplay::displayChosenLetter(char input) {
+ int index;
+    
+    if (input >= 'a' && input <= 'z') {
+        index = input - 'a'; // Letras minúsculas
+    } else if (input >= 'A' && input <= 'Z') {
+        index = input - 'A'; // Letras maiúsculas
+    } else if (input >= '0' && input <= '9') {
+        index = input - '0' + 26; // Números de 0 a 9
+    } else {
+        // Caractere inválido
+        return;
+    }
+    
+    displayLetter(index); // Exibe a letra ou número correspondente na matriz de LEDs
+}
+
+// Exibir todo o alfabeto na matriz
+void NextDisplay::displayAlphabet() {
+  for (int k = 0; k < 26; k++) { // Loop para percorrer todas as letras do alfabeto
+    for (int t = 0; t < 500; t++) { // Exibir cada letra por um tempo (ajuste conforme necessário)
+      displayLetter(k);
+    }
+    delay(500); // Pequena pausa entre as letras
+  }
+}
---
+++ src/NextDisplay.h
@@ -0,0 +1,26 @@
+/*////////////////////////////////////////////////////////////////////////////////
+ * Arduino code to display characters on a 8x5 LED matriz.
+ * Designed for a Integrated Product Development project of the college FATEC Jessen Vidal.
+ * Author: João Álvaro                                    
+/*////////////////////////////////////////////////////////////////////////////////
+
+#ifndef NextDisplay_h
+#define NextDisplay_h
+
+#include "Arduino.h"
+
+extern bool alphabet[36][8][5]; // Declaração externa da matriz para letras do alfabeto
+
+class NextDisplay {
+  public:
+    NextDisplay(); // Construtor da classe
+    void displayLetter(int letterIndex); // Exibe uma letra na matriz de LEDs
+    void displayChosenLetter(char input); // Exibe a letra escolhida na matriz de LEDs
+    void displayAlphabet(); // Exibe todo o alfabeto na matriz de LEDs
+  private:
+    const int rows[8] = {2, 3, 4, 5, 6, 7, 8, 9}; // Pin Linhas
+    const int cols[5] = {10, 11, 12, A0, A1}; // Pin Colunas
+};
+
+#endif
+
